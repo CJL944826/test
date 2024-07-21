7fd90548e61968eb472b57bfc72fcbf24b36ddf7
CJL944826 <czl.1845772930@qq.com>
2024-07-21 12:16:05 +0800

    1

---
+++ test1.java
@@ -0,0 +1,11 @@
+/**
+ * @author czl18
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/7/21 12:08
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
